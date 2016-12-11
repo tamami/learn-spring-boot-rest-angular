73ff46b33b39ea6a55acc5264dd9798de3d6a09e
tamami <tamami.oka@gmail.com>
2016-12-11 18:09:40 +0700

    update content

---
+++ src/main/java/lab/aikibo/springservice/Task.java
@@ -0,0 +1,41 @@
+package lab.aikibo.springservice;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * Created by tamami on 11/12/16.
+ */
+@Data
+@Entity
+@Table(name="task_list")
+public class Task {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name="task_id")
+  private int id;
+
+  @Column(name="task_name")
+  private String taskName;
+
+  @Column(name="task_description")
+  private String taskDescription;
+
+  @Column(name="task_priority")
+  private String taskPriority;
+
+  @Column(name="task_status")
+  private String taskStatus;
+
+  @Column(name="task_archived")
+  private int taskArchived = 0;
+
+  @Override
+  public String toString() {
+    return "Task [id=" + id + ", taskName=" + taskName + ", taskDescription=" + taskDescription +
+        ", taskPriority=" + taskPriority + ",taskStatus=" + taskStatus + "]";
+  }
+
+}
---
+++ src/main/java/lab/aikibo/springservice/TaskRepository.java
@@ -0,0 +1,10 @@
+package lab.aikibo.springservice;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * Created by tamami on 11/12/16.
+ */
+@RepositoryRestResource
+public interface TaskRepository {
+}
